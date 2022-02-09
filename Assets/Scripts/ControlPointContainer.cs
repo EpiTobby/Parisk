082473f629c44e91b0c3e1ea294bba776974816c
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-02-09 20:16:09 +0100

    control points: add points

--- Assets/Scripts/ControlPointContainer.cs
+++ Assets/Scripts/ControlPointContainer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Parisk;
 
@@ -16,5 +17,44 @@ public class ControlPointContainer
         dictionary.Add(Side.Communards, 0); // FIXME (random)
         dictionary.Add(Side.Versaillais, 0); // FIXME (random)
         return new ControlPointContainer(dictionary);
+    }
+
+    public int GetPointsFor(Side side)
+    {
+        return _points[side];
+    }
+
+    public int GetAbsenteeism()
+    {
+        return 100 - GetPointsFor(Side.Communards) - GetPointsFor(Side.Versaillais);
+    }
+
+    public void AddPointsTo(Side side, int amount, PointSource source = PointSource.Mixed)
+    {
+        amount = Math.Min(amount, 100 - _points[side]);
+        _points[side] += amount;
+
+        Side adversary = side == Side.Communards ? Side.Versaillais : Side.Communards;
+        switch (source)
+        {
+            case PointSource.Adversary:
+                _points[adversary] = Math.Max(0, _points[adversary] - amount);
+                break;
+            case PointSource.Absenteeism:
+            {
+                if (GetAbsenteeism() < 0)
+                    _points[adversary] += GetAbsenteeism();
+                break;
+            }
+            case PointSource.Mixed:
+            {
+                _points[adversary] = Math.Max(0, _points[adversary] - amount / 2);
+                if (GetAbsenteeism() < 0)
+                    _points[adversary] += GetAbsenteeism();
+                break;
+            }
+            default:
+                throw new Exception();
+        }
     }
 }
