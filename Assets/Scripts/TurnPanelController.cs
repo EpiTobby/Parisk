ce9a907a5761179e367e052a1f4e105414a3e204
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-02-12 15:03:20 +0100

    require 2 actions to finish turn

--- Assets/Scripts/TurnPanelController.cs
+++ Assets/Scripts/TurnPanelController.cs
@@ -6,27 +6,44 @@ public class TurnPanelController : MonoBehaviour, EventObserver
 {
     [SerializeField] private Button nextTurnButton;
     private int _actionCount = 0;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        nextTurnButton.interactable = false;
         GameController.Get().RegisterEventObserver(this);
+        UpdateNextTurnButton();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void OnAction()
     {
         _actionCount++;
+        UpdateNextTurnButton();
+    }
+
+    private void UpdateNextTurnButton()
+    {
+        var buttonText = nextTurnButton.GetComponentInChildren<Text>();
+        if (_actionCount < 2)
+        {
+            buttonText.text = _actionCount + "/2 Actions";
+            nextTurnButton.interactable = false;
+        }
+        else
+        {
+            buttonText.text = "Tour suivant";
+            nextTurnButton.interactable = true;
+        }
     }
 
     public void OnClickNextTurn()
     {
+        _actionCount = 0;
+        UpdateNextTurnButton();
         GameController.Get().endActivePlayerTurn();
     }
 }
